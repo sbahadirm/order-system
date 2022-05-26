5641f16ff8da3100448ed4023a599cc4a74db566
sbahadirm <sbahadirm@gmail.com>
2022-05-26 23:52:06 +0300

    Initial commit

---
+++ src/test/java/com/bahadirmemis/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadirmemis.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
