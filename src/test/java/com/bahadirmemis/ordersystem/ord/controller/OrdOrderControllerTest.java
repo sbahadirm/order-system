53eacc53b4d30b7196434f477cf07298045b5552
sbahadirm <sbahadirm@gmail.com>
2022-06-03 03:05:53 +0300

    add integration test for OrdOrderController

---
+++ src/test/java/com/bahadirmemis/ordersystem/ord/controller/OrdOrderControllerTest.java
@@ -0,0 +1,143 @@
+package com.bahadirmemis.ordersystem.ord.controller;
+
+import com.bahadirmemis.ordersystem.BaseTest;
+import com.bahadirmemis.ordersystem.ord.dto.OrdOrderSaveRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Bahadır Memiş
+ * @since 1.0.0
+ */
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class OrdOrderControllerTest extends BaseTest {
+
+    private static final String BASE_PATH = "/api/v1/orders";
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    void save() throws Exception {
+
+        OrdOrderSaveRequestDto ordOrderSaveRequestDto = OrdOrderSaveRequestDto.builder()
+                .orderDate(new Date())
+                .addressId(12121L)
+                .customerId(18L)
+                .paidAmount(BigDecimal.valueOf(100))
+                .productIdList(Arrays.asList(1L, 2L))
+                .deliveryCompany("MNG")
+                .build();
+
+        String content = objectMapper.writeValueAsString(ordOrderSaveRequestDto);
+
+        MvcResult result = mockMvc.perform(
+                post(BASE_PATH).content(content).contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+
+    }
+
+    @Test
+    void getOrdOrderById() throws Exception {
+
+        MvcResult result = mockMvc.perform(
+                get(BASE_PATH+ "/1")
+                        .content("1L")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+    }
+
+    @Test
+    void getOrdOrderByCustomerId() throws Exception {
+
+        MvcResult result = mockMvc.perform(
+                get(BASE_PATH+ "/customers/18")
+                        .content("18L")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+    }
+
+    @Test
+    void findAll() throws Exception {
+
+        MvcResult result = mockMvc.perform(
+                get(BASE_PATH).contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+    }
+
+    @Test
+    void delete() throws Exception {
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.delete(BASE_PATH+ "/1")
+                        .content("1L")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+    }
+
+    @Test
+    void cancel() throws Exception {
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.patch(BASE_PATH+ "/cancel/2")
+                        .content("2L")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        boolean isSuccess = isSuccess(result);
+
+        assertTrue(isSuccess);
+
+    }
+}
